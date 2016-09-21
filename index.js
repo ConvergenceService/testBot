9e64231c1642dadbe6e576ed7a5718e9935ddef5
convergence.service <convergence.service@gmail.com>
2016-09-22 00:13:44 +0900

    seventh commit

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
+/*-----------------------------------------------------------------------------
+A simple "Hello World" bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
+
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    appId: 'cstestbot', 
-    appSecret: '5woRjPoAgighouDx0HpnQFA'
-};
-
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
-// Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+//=========================================================
+// Bots Dialogs
+//=========================================================
 
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+bot.dialog('/', function (session) {
+    session.send("Hello World");
 });
